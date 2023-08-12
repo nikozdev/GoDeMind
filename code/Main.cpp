16e0fce5c77c64ea71e04c3c8c6fabf5deb72d9f
nikozdev <nikzodev@nikzodev.net>
2023-08-12 21:23:41 +0300

    !envi!test!code!make!refactor!clean!organize!structure
    [code/Main.cpp]
    > managing tests by command line arguments
    > using option-defined macros
    [envi/CMakeLists.txt]
    > added HelloWorld test
    > added target general compile definitions
    [envi/clang-format.yaml]
    > removed the last breaking setting - QualifierOrder "friend" entry

--- code/Main.cpp
+++ code/Main.cpp
@@ -1,12 +1,49 @@
-#ifndef dGofDeMin_Main_Cxx
-#define dGofDeMin_Main_Cxx
-
+#ifndef dGofDeMin_Main_cpp
+#define dGofDeMin_Main_cpp
+//headers
+//-//standard
+#include <string_view>
 #include <iostream>
-
-int main(int vArgC, const char** vArgV, const char** vEnvi)
+//content
+namespace nGofDeMin
 {
-  std::cout << "HelloWorld" << std::endl;
-  return 0;
-}
-
-#endif//dGofDeMin_Main_Cxx
+}//namespace nGofDeMin
+#if defined(dGofDeMin_MakeTexe)
+//headers
+#include <unordered_map>
+#include <functional>
+//typedef
+using tTestKey = std::string_view;
+using tTestCmd = std::function<int(void)>;
+using tTestTab = std::unordered_map<tTestKey, tTestCmd>;
+//forward
+#if defined(dGofDeMin_MakeTest)
+extern const tTestTab vTestTab;
+#endif//ifd(dGofDeMin_MakeTest)
+//actions
+int main(int vArgC, const char **vArgV, const char **vEnvi)
+{
+#if defined(dGofDeMin_MakeTest)
+	if(vArgC > 1)
+	{
+		auto vTestRef = vTestTab.find(vArgV[1]);
+		if(vTestRef != vTestTab.end())
+		{
+			return vTestRef->second();
+		}
+	}
+#endif//ifd(dGofDeMin_MakeTest)
+	return EXIT_SUCCESS;
+}//main
+#endif//ifd(dGofDeMin_MakeTexe)
+#if defined(dGofDeMin_MakeTest)
+const tTestTab vTestTab = {
+	{"tHelloWorld",
+	 []()
+	 {
+		 std::cout << "HelloWorld" << std::endl;
+		 return EXIT_SUCCESS;
+	 }},
+};//vTestTab
+#endif//ifd(dGofDeMin_MakeTest)
+#endif//dGofDeMin_Main_cpp
